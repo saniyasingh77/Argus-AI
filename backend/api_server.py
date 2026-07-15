e15b4c5ef03a9e9df143883fd7a0c194421e80c7
saniyasingh77 <singhsaniya672@gmail.com>
2026-07-15 13:19:02 +0530

    Removed generated files

--- backend/api_server.py
+++ backend/api_server.py
@@ -286,6 +286,5 @@ def delete_user(user_id):
 # ================= RUN =================
 
 if __name__ == "__main__":
-
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)      
     
